3dfa300cd399c8d0434be3d172efb23d2dc13558
SirAlbert <edgar.betancourt@uabc.edu.mx>
2026-02-25 18:49:18 -0800

    Cola

--- src/test/Main.c
+++ src/test/Main.c
@@ -3,6 +3,7 @@
 #include <lista.h>
 #include <listadoble.h>
 #include <pila.h>
+#include <cola.h>
 #include "Alumno.h"
 
 void imprimirEntero(void *dato);
@@ -11,12 +12,26 @@ void imprimirEntero(void *dato);
 int main()
 {
 	Pila pila = {NULL,0,4,imprimirEntero,NULL};
+	Cola cola = {NULL,NULL,0,4,imprimirEntero};
 	int arreglo[6] = {1,5,4,3,5,9};
 	for(int i=0; i<6;i++)
+	{
 		pushDato(&pila,&arreglo[i]);
+		agregarCola(&cola,&arreglo[i]);
+	}
 	
 	imprimirPila(pila);
+	imprimirCola(cola);
 	
+	for(int i=0; i<3;i++)
+	{
+		popDato(&pila);
+		removerCola(&cola);
+	}
+	
+	
+	imprimirPila(pila);
+	imprimirCola(cola);
 
 	return 0;
 }
